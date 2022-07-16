dcc34a46baeefe3d931fb5a2338c08c94c4c7f59
David Smith <x300bps@icloud.com>
2022-07-16 15:09:57 -0400

    Added 'predator vision' color palette and made it default.  Fixed bug that I introduced that didn't correctly display last captured image.

--- Pybadge_Thermal_Image_Recording.ino
+++ Pybadge_Thermal_Image_Recording.ino
@@ -96,6 +96,35 @@ const PROGMEM uint8_t snowIcon[] = {
 0x15, 0x00, 0x4E, 0x40, 0xC4, 0x60, 0x75, 0xC0, 0x9F, 0x20, 0x0E, 0x00,
 0x0E, 0x00, 0x9F, 0x20, 0x75, 0xC0, 0xC4, 0x60, 0x4E, 0x40, 0x15, 0x00};
 
+// Palette from adafruit project guide https://learn.adafruit.com/thermal-camera-with-display/overview that reminds me of the Predators thermal vision
+const uint16_t predatorPalette[] = {0x480F,
+0x400F,0x400F,0x400F,0x4010,0x3810,0x3810,0x3810,0x3810,0x3010,0x3010,
+0x3010,0x2810,0x2810,0x2810,0x2810,0x2010,0x2010,0x2010,0x1810,0x1810,
+0x1811,0x1811,0x1011,0x1011,0x1011,0x0811,0x0811,0x0811,0x0011,0x0011,
+0x0011,0x0011,0x0011,0x0031,0x0031,0x0051,0x0072,0x0072,0x0092,0x00B2,
+0x00B2,0x00D2,0x00F2,0x00F2,0x0112,0x0132,0x0152,0x0152,0x0172,0x0192,
+0x0192,0x01B2,0x01D2,0x01F3,0x01F3,0x0213,0x0233,0x0253,0x0253,0x0273,
+0x0293,0x02B3,0x02D3,0x02D3,0x02F3,0x0313,0x0333,0x0333,0x0353,0x0373,
+0x0394,0x03B4,0x03D4,0x03D4,0x03F4,0x0414,0x0434,0x0454,0x0474,0x0474,
+0x0494,0x04B4,0x04D4,0x04F4,0x0514,0x0534,0x0534,0x0554,0x0554,0x0574,
+0x0574,0x0573,0x0573,0x0573,0x0572,0x0572,0x0572,0x0571,0x0591,0x0591,
+0x0590,0x0590,0x058F,0x058F,0x058F,0x058E,0x05AE,0x05AE,0x05AD,0x05AD,
+0x05AD,0x05AC,0x05AC,0x05AB,0x05CB,0x05CB,0x05CA,0x05CA,0x05CA,0x05C9,
+0x05C9,0x05C8,0x05E8,0x05E8,0x05E7,0x05E7,0x05E6,0x05E6,0x05E6,0x05E5,
+0x05E5,0x0604,0x0604,0x0604,0x0603,0x0603,0x0602,0x0602,0x0601,0x0621,
+0x0621,0x0620,0x0620,0x0620,0x0620,0x0E20,0x0E20,0x0E40,0x1640,0x1640,
+0x1E40,0x1E40,0x2640,0x2640,0x2E40,0x2E60,0x3660,0x3660,0x3E60,0x3E60,
+0x3E60,0x4660,0x4660,0x4E60,0x4E80,0x5680,0x5680,0x5E80,0x5E80,0x6680,
+0x6680,0x6E80,0x6EA0,0x76A0,0x76A0,0x7EA0,0x7EA0,0x86A0,0x86A0,0x8EA0,
+0x8EC0,0x96C0,0x96C0,0x9EC0,0x9EC0,0xA6C0,0xAEC0,0xAEC0,0xB6E0,0xB6E0,
+0xBEE0,0xBEE0,0xC6E0,0xC6E0,0xCEE0,0xCEE0,0xD6E0,0xD700,0xDF00,0xDEE0,
+0xDEC0,0xDEA0,0xDE80,0xDE80,0xE660,0xE640,0xE620,0xE600,0xE5E0,0xE5C0,
+0xE5A0,0xE580,0xE560,0xE540,0xE520,0xE500,0xE4E0,0xE4C0,0xE4A0,0xE480,
+0xE460,0xEC40,0xEC20,0xEC00,0xEBE0,0xEBC0,0xEBA0,0xEB80,0xEB60,0xEB40,
+0xEB20,0xEB00,0xEAE0,0xEAC0,0xEAA0,0xEA80,0xEA60,0xEA40,0xF220,0xF200,
+0xF1E0,0xF1C0,0xF1A0,0xF180,0xF160,0xF140,0xF100,0xF0E0,0xF0C0,0xF0A0,
+0xF080,0xF060,0xF040,0xF020,0xF800,};
+
 
 Adafruit_MLX90640 mlx;
 Adafruit_Arcada arcada;
@@ -297,14 +326,20 @@ void loop()
   arcada.display->setTextSize(1);
   arcada.display->setCursor(10, 99);
   arcada.display->setTextColor(0xFFFF ^ colorPal[0]);   // Contrasting text color for coldest value
+  // Special case - use white for predator palette  
+  if (paletteNum == 1)
+    arcada.display->setTextColor(0xFFFF);
   arcada.display->print(celsiusFlag ? lowPix : lowPix * 1.8 + 32.0);  // Print Celsius or Fahrenheit
 
   arcada.display->setCursor(120, 99);
   arcada.display->setTextColor(0xFFFF ^ colorPal[255]); // Contrast text for hottest value
+  // Special case - use white for predator palette  
+  if (paletteNum == 1)
+    arcada.display->setTextColor(0xFFFF);  
   arcada.display->print(celsiusFlag ? highPix : highPix * 1.8 + 32.0);  // Print Celsius or Fahrenheit
 
   arcada.display->setCursor(65, 99);
-  if((mlx90640To[400] < (colorLow + colorHigh) * 0.5) == (paletteNum < 3))
+  if((mlx90640To[400] < (colorLow + colorHigh) * 0.5) == (paletteNum < 4))
     arcada.display->setTextColor(0xFFFF);               // A contrasting text color for center temp
   else
     arcada.display->setTextColor(0x0000);
@@ -463,7 +498,11 @@ void loadPalette(uint16_t palNumber) {
   float fleX, fleK;
 
   switch(palNumber) {
-    case 1:  // Compute ironbow palette
+    case 1: // "Predator vision" palette
+      for(x = 0; x < 256; ++x)
+        colorPal[x] = predatorPalette[x];
+      break;      
+    case 2:  // Compute ironbow palette
       for(x = 0; x < 256; ++x) {
         fleX = (float)x / 255.0;
 
@@ -483,7 +522,7 @@ void loadPalette(uint16_t palNumber) {
         colorPal[x] += (uint16_t)fleK & 0x001F;  // Bottom 5 bits define blue
       }
       break;
-    case 2:  // Compute quadratic "firebow" palette
+    case 3:  // Compute quadratic "firebow" palette
       for(x = 0; x < 256; ++x) {
         fleX = (float)x / 255.0;
 
@@ -499,8 +538,8 @@ void loadPalette(uint16_t palNumber) {
         fleK = fleX < 0.5 ? 0.0 : (fleX - 0.5) * (fleX - 0.5) * 123.0;
         colorPal[x] += (uint16_t)fleK & 0x001F;  // Bottom 5 bits define blue
       }
-      break;
-    case 3:  // Compute "alarm" palette
+      break;      
+    case 4:  // Compute "alarm" palette
       for(x = 0; x < 256; ++x) {
         fleX = (float)x / 255.0;
 
@@ -514,7 +553,7 @@ void loadPalette(uint16_t palNumber) {
         colorPal[x] += (uint16_t)fleK & 0x001F;  // Bottom 5 bits define blue
       }
       break;
-    case 4:  // Compute negative gray palette, black hot
+    case 5:  // Compute negative gray palette, black hot
       for(x = 0; x < 256; ++x)
         colorPal[255 - x] = (((uint16_t)x << 8) & 0xF800) + (((uint16_t)x << 3) & 0x07E0) + (((uint16_t)x >> 3) & 0x001F);
       break;
@@ -673,7 +712,7 @@ void recallLastBMP(uint16_t bgColor) {  // Display 8-bit values left in buffer f
 
   // Restore values from pixelArray back to mlx buffer for interpolation
   for (uint16_t u16Index = 0; u16Index < MLX_ROWS*MLX_COLS; u16Index++)      
-    mlx90640To[u16Index] = pixelArray[3*u16Index];
+    mlx90640To[u16Index] = pixelArray[3*u16Index + 2];
 
   // Perform bilinear interpolation (quadruples the number of active display points: (32x24) -> (64x48))
   bilinear_interpolation(mlx90640To, terpArray); 
@@ -771,7 +810,7 @@ boolean menuLoop(uint16_t bgColor) {  // Lay out a menu screen, interact to chan
           buttonRfunc = (buttonRfunc + 1) % 3; // Step through button functions
           break;
         case 3:
-          loadPalette(paletteNum = (paletteNum + 1) % 5); // Step through various color palettes
+          loadPalette(paletteNum = (paletteNum + 1) % 6); // Step through various color palettes
           break;
         case 4:
           thermRange = (thermRange + 1) % 6; // Step through various temp range presets
@@ -856,16 +895,19 @@ void menuLines(int lineNumber, int scrollPos) {  // Screen print a single line i
                                         8, colorPal[map(xPos, 0, 71, 0, 255)]);
         switch(paletteNum) {
           case 1:
-            arcada.display->print("IRONBOW");
+            arcada.display->print("PREDATOR");
             break;
           case 2:
-            arcada.display->print("FIREBOW");
+            arcada.display->print("IRONBOW");
             break;
           case 3:
+            arcada.display->print("FIREBOW");
+            break;
+          case 4:
             arcada.display->setTextColor(0x0000);    // Black text for reverse contrast
             arcada.display->print("ALARM");
             break;
-          case 4:
+          case 5:
             arcada.display->setTextColor(0x0000);    // Black text
             arcada.display->print("BLACK HOT");
             break;
